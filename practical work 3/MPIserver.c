62b355c9fbb6e8cb4f0a1d6031ad226918924348
pkcuong <125652433+pkcuong@users.noreply.github.com>
2024-03-27 09:55:26 +0700

    Update MPIserver.c

--- practical work 3/MPIserver.c
+++ practical work 3/MPIserver.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mpi.h>
+#include <fcntl.h>
 
-#define MAX_FILENAME_LENGTH 256
-#define CHUNK_SIZE 1024
+#define MAX 1024
+#define PORT 8080
 
-void send_file(const char *filename, int dest_rank) {
-    char buff[CHUNK_SIZE];
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file.\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
+void send_file(char *filename) {
+    char buff[MAX];
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        printf("Error opening file.\n");
+        return;
     }
 
-    int bytes_read;
-    while ((bytes_read = fread(buff, sizeof(char), CHUNK_SIZE, file)) > 0) {
-        MPI_Send(buff, bytes_read, MPI_CHAR, dest_rank, 0, MPI_COMM_WORLD);
+    while (1) {
+        int bytes_read = read(fd, buff, MAX);
+        if (bytes_read <= 0) break;
+        MPI_Send(buff, bytes_read, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
-
-    fclose(file);
+    close(fd);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
+    int rank, size;
     MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    if (rank == 0) {
+        printf("Server listening..\n");
 
-    char filename[MAX_FILENAME_LENGTH] = "MPI_server_file.txt"; 
+        MPI_Status status;
+        char filename[MAX];
+        MPI_Recv(filename, MAX, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
 
-    if (world_rank == 0) {
-        // master 
-        int num_processes;
-        MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-
-        for (int dest_rank = 1; dest_rank < num_processes; dest_rank++) {
-            send_file(filename, dest_rank);
-        }
-    } else {
-        // slave
-        receive_file(filename);
+        send_file(filename);
     }
 
     MPI_Finalize();
